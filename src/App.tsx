5e6cf5f8b0dd5bf44396778721f4f84682d55572
Sarajit Mandal <bdsarajit499@gmail.com>
2025-07-02 19:46:12 +0600

    setup redux and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Button } from "./components/ui/button"
+import { decrement, increment } from "./redux/features/counter/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hooks"
 
 
 
 function App() {
-
+const value = useAppSelector(state => state.counter.value)
+const dispatchEvent = useAppDispatch()
 
   return (
    <>
-   assignment-4
+   <div>
+    <button onClick={()=>dispatchEvent(decrement())} className="text-3xl font-bold mx-5">-</button>
+    <button className="text-3xl font-bold mx-5">{value
+      }</button>
+    <Button  onClick={()=>dispatchEvent(increment())} className="text-3xl font-bold mx-5">+</Button>
+   </div>
    </>
   )
 }
